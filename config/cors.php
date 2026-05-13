72896c7845621d623efe9a61fcc9e05ce8b2ef43
tahashabaan202-debug <tahashabaan202@gmail.com>
2026-05-14 00:55:32 +0300

    update cors file

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,14 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_values(array_filter(array_map(
+        'trim',
+        explode(',', (string) env('CORS_ALLOWED_ORIGINS', implode(',', [
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+            'https://graduation.ct.ws',
+        ])))
+    ))),
 
     'allowed_origins_patterns' => [],
 
