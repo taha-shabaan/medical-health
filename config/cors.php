b20083acc3d8c1de86ad46ba407ca3708d1329a8
tahashabaan202-debug <tahashabaan202@gmail.com>
2026-05-14 01:01:30 +0300

    update cors file

--- config/cors.php
+++ config/cors.php
@@ -23,8 +23,7 @@ return [
         'trim',
         explode(',', (string) env('CORS_ALLOWED_ORIGINS', implode(',', [
             'http://localhost:5173',
-            'http://127.0.0.1:5173',
-            'https://graduation.ct.ws',
+            'https://graduation-project-ivory.vercel.app/',
         ])))
     ))),
 
